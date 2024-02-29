1643f359f01dbd41c55a7fcd6e315d14ce066225
Dmitrii Stepanov <dstepanov200498@gmail.com>
2024-02-29 18:17:46 +0400

    проверка сделана

--- check_result.py
+++ check_result.py
@@ -1,24 +1,27 @@
 import numpy as np
 
 def read_matrix(size, filename):
-    f = open(filename, 'r')
     A = np.zeros((size, size))
-    
-    f.close()
+    with open(filename) as file:
+        lines = [int(line.rstrip()) for line in file]
+    k = 0    
+    for i in range(size):
+        for j in range(size):
+            A[i][j] = int(lines[k])
+            k +=1
+    file.close()
     return A
 
-def check_matrix(size, filename):
-    res = open(filename, 'r')
+def check_matrix(size):
+    res = open('check_result', 'a')
     A = read_matrix(size, 'matrix_A'+ str(size) + '.txt')
     B = read_matrix(size, 'matrix_B'+ str(size) + '.txt')
     C1 = read_matrix(size, 'matrix_C'+ str(size) + '.txt')
     C = np.matmul(A, B)
-    res.write('Результат сравнения матриц размера: ' + str(size) + ' ' + str(np.array_equal(C, C1)))
+    res.write('Результат сравнения матриц размера: ' + str(size) + ' ' + str(np.array_equal(C, C1)) + '\n')
     res.close()
     
 if __name__ == "__main__":
     list_of_size = [10, 20, 50, 100, 200, 500, 750, 1000]
-    list_of_filenames = ['matrix_A', 'matrix_B']
     for i in list_of_size:
-        for j in list_of_filenames:
-            check_matrix(i, j + str(i) + '.txt')
+        check_matrix(i)
